fba8c2bae250007f41398e1c7b1f2b2f8664366f
ebenezer pmg <ebenezer.ganesan@metrics.ca>
2021-08-03 11:27:39 +0530

    interleaved test addition

--- apb_sequence.sv
+++ apb_sequence.sv
@@ -28,7 +28,7 @@ class apb_sequence extends uvm_sequence#(apb_item);
 	  for ( int i= 0; i < 10 ; i++) begin 
             `uvm_info ("MYINFO","passing packet with write data to driver ", UVM_LOW ); 
 	  `uvm_do_with (req,
-		          {req.PDATA ==  32'hABAB;
+		          {req.PDATA ==  i;
 		           req.PWRITE == 1'b1; 
 			   req.PADDR ==  i; 
 		   })
@@ -38,7 +38,7 @@ class apb_sequence extends uvm_sequence#(apb_item);
 	   for (int i= 0; i <10 ; i++) begin 
             `uvm_info ("MYINFO","passing packet2 with read data to driver ", UVM_LOW ); 
 	  `uvm_do_with (req,
-		          {req.PDATA ==  32'h0;
+		          {req.PDATA ==  i;
 		           req.PWRITE == 1'b0; 
 			   req.PADDR == i;
 		   })
@@ -58,6 +58,52 @@ class apb_sequence_cont_write_read  extends uvm_sequence#(apb_item);
 	endfunction
 
 	`uvm_sequence_utils(apb_sequence_cont_write_read,apb_sequencer)
+
+	virtual task pre_body();
+	if (starting_phase != null)
+		starting_phase.raise_objection(this, {"Runnning sequence'",get_full_name(),"'"});
+        endtask
+	virtual task post_body();
+	if (starting_phase != null)
+		starting_phase.drop_objection(this, {"Completed sequence'",get_full_name(),"'"});
+        endtask
+	virtual task body();
+            `uvm_info ("MYINFO","Entered Seq ", UVM_LOW );
+	    // req.print();
+	  for ( int i= 0; i < 10 ; i++) begin
+            `uvm_info ("MYINFO","passing packet with write data to driver ", UVM_LOW );
+	  `uvm_do_with (req,
+		          {req.PDATA ==  32'hABAB;
+		           req.PWRITE == 1'b1;
+			   req.PADDR ==  i;
+		   })
+	   get_response(rsp);
+	   // rsp.print();
+           end
+	   for (int i= 0; i <10 ; i++) begin
+            `uvm_info ("MYINFO","passing packet2 with read data to driver ", UVM_LOW );
+	  `uvm_do_with (req,
+		          {req.PDATA ==  32'h0;
+		           req.PWRITE == 1'b0;
+			   req.PADDR == i;
+		   })
+            `uvm_info ("MYINFO","waiting for response from Driver ", UVM_LOW );
+	   get_response(rsp);
+            `uvm_info ("MYINFO","Got response from Driver ", UVM_LOW );
+          end
+	    //rsp.print();
+         endtask
+endclass
+
+class apb_sequence_interlvd_write_read  extends uvm_sequence#(apb_item);
+
+	apb_item req;
+
+	function new(string name="apb_sequence_interlvd_write_read");
+		super.new(name);
+	endfunction
+
+	`uvm_sequence_utils(apb_sequence_interlvd_write_read,apb_sequencer)
 
 	virtual task pre_body();
 	if (starting_phase != null)
--- apb_test.sv
+++ apb_test.sv
@@ -59,6 +59,36 @@ class apb_cont_write_read_test extends uvm_test;
 
 endclass : apb_cont_write_read_test
 
+class apb_interlvd_write_read_test extends uvm_test;
+
+	`uvm_component_utils(apb_interlvd_write_read_test)
+
+	apb_env env;
+	apb_sequence_interlvd_write_read  seq;
+
+	function new(string name = "apb_interlvd_write_read_test",uvm_component parent=null);
+		super.new(name,parent);
+	endfunction : new
+
+	virtual function void build_phase(uvm_phase phase);
+	  super.build_phase(phase);
+	  env = apb_env::type_id::create("env",this);
+	  seq = apb_sequence_interlvd_write_read::type_id::create("seq");
+
+	endfunction :build_phase
+
+	task run_phase(uvm_phase phase);
+		phase.raise_objection(this);
+	fork
+	        seq.start(env.apb_agt.sequencer);
+	        #10000;
+        join_any
+		phase.drop_objection(this);
+         endtask :run_phase
+
+endclass : apb_interlvd_write_read_test
+
+
 class apb_write_read_test extends uvm_test;
 
 	`uvm_component_utils(apb_write_read_test)
